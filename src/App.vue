90bcccf00b14c78f9db4b1e6fd05f9d43fe52d39
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2023-04-14 13:15:55 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<script setup>
+<!--<script setup>
 import { ref } from 'vue';
+
 
 const title = ref([]);
 const content = ref([]);
@@ -16,9 +17,8 @@ fetch('https://process.mg-visions.com/wp-json/wp/v2/events', {
   })
   .then((response) => {
     console.log(response);
-    title.value = response.data.title;
-    content.value = response.data.content;
-    
+    title.value = response[0].title.rendered;
+    content.value = response[0].content.rendered;
   });
 </script>
 
@@ -27,12 +27,33 @@ fetch('https://process.mg-visions.com/wp-json/wp/v2/events', {
     <h3>Pros</h3>
       <ul>
         <li v-for="item in title">Single title goes here
-          {{ item }}
+          <h4>{{ title }}</h4>
+          <p>{{ content }}</p>
         </li>
       </ul>
   </div>
 </template>
 
 <style scoped></style>
+-->
+<script setup>
+import { ref } from 'vue';
 
+const events = ref([]);
 
+fetch('https://process.mg-visions.com/wp-json/wp/v2/events')
+  .then(response => response.json())
+  .then(data => events.value = data);
+</script>
+
+<template>
+  <div>
+    <h3>Pros</h3>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        <h4>{{ event.title.rendered }}</h4>
+        <p>{{ event.content.rendered }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
