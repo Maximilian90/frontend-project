f000bb9291365795843a1d2fab82505f6206c5e7
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2023-04-18 22:00:28 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
-<!--<script setup>
-import { ref } from 'vue';
 
-
-const title = ref([]);
-const content = ref([]);
-/*const thumbnail = ref('Event img')
-const pros = ref([]);
-const cons = ref([]);*/
-
-
-fetch('https://process.mg-visions.com/wp-json/wp/v2/events', {
-  method: 'get',
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    console.log(response);
-    title.value = response[0].title.rendered;
-    content.value = response[0].content.rendered;
-  });
-</script>
-
-<template>
-  <div>
-    <h3>Pros</h3>
-      <ul>
-        <li v-for="item in title">Single title goes here
-          <h4>{{ title }}</h4>
-          <p>{{ content }}</p>
-        </li>
-      </ul>
-  </div>
-</template>
-
-<style scoped></style>
--->
 <script setup>
 import { ref } from 'vue';
+import CustomField from './components/CustomField.vue';
 
 const events = ref([]);
 
@@ -46,15 +10,14 @@ fetch('https://process.mg-visions.com/wp-json/wp/v2/events')
   .then(data => events.value = data);
 
 function getFeaturedImageUrl(event) {
-const content = event.content.rendered;
-const match = content.match(/<img[^>]+src="([^">]+)"/);
+  const content = event.content.rendered;
+  const match = content.match(/<img[^>]+src="([^">]+)"/);
   if (match) {
     return match[1];
   } else {
     return '';
   }
 }
-
 </script>
 
 <template>
@@ -63,6 +26,7 @@ const match = content.match(/<img[^>]+src="([^">]+)"/);
     <ul>
       <li v-for="event in events" :key="event.id">
         <h4>{{ event.title.rendered }}</h4>
+        <CustomField :customFieldValue="event.custom_field_value" />
         <img :src="getFeaturedImageUrl(event)" :alt="event.title.rendered" />
         <p>{{ event.content.rendered }}</p>
       </li>
---
+++ src/components/CustomField.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  customFieldValue: {
+    type: String,
+    required: true,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <p>{{ customFieldValue }}</p>
+</template>
